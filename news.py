f95ee287e9626807f2224ff2f2cfbd2f130e3108
ruslan.liska <ruslan.liska@globallogic.com>
2022-05-08 15:44:52 +0300

    Updated news functionality

--- news.py
+++ news.py
@@ -6,17 +6,14 @@ from bs4 import BeautifulSoup
 
 def get_article():
     """Parse for last article and returns it."""
-    bbc_request = requests.get('https://www.bbc.com/ukrainian')
-    soup = BeautifulSoup(bbc_request.text, "html.parser")
-    raw_article = soup.find_all('div', {'class': "bbc-1vo75s2-TextGridItem e19k1v2h0"})[0].find_all(text=True, recursive=True)
+    page = requests.get('https://www.pravda.com.ua/')
+    soup = BeautifulSoup(page.content, "html.parser")
 
-    title = raw_article[0]
-    description = raw_article[1]
-    publish_time = raw_article[2]
-    href = soup.find_all('div', {'class': 'bbc-1vo75s2-TextGridItem e19k1v2h0'})[0].find('a', {'class': 'bbc-11m194t-Link evnt13t0'})['href']
-    link = f' https://www.bbc.com/{href}'
-    article = f"""⚠️ <b>Тема</b>:  {title}
-                  \n📌 <b>Короткий опис</b>:  {description}
-                  \n🕒 <b>Опубліковано</b>:  {publish_time}
+    articles = soup.find_all('div', {'class': 'article_header'})[0].find_all(text=True, recursive=True)
+
+    href = soup.find_all('div', {'class': 'article_header'})[0].find('a')['href']
+    link = f'https://www.pravda.com.ua{href}'
+    print(link)
+    article = f"""⚠️ <b>Тема</b>:  {articles[0]}
                   \n➡️ <b>Повний текст</b>: {link}"""
     return article
